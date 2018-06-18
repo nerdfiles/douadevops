fb212dd6aef7cf0e225745b2a7397a7d2fa9f41b
nerdfiles <nerdfiles@gmail.com>
2018-06-18 16:07:44 -0500

    Cleanup

---
+++ datastructures/trees.mm
@@ -0,0 +1,28 @@
+
+<map version="0.9.0">
+    <node TEXT="trees">
+        <node TEXT="right"/>
+        <node TEXT="__height__"/>
+        <node TEXT="Node"/>
+        <node TEXT="__decode__"/>
+        <node TEXT="__printLeft__"/>
+        <node TEXT="__printRight__"/>
+        <node TEXT="Kinds">
+            <node TEXT="TopView"/>
+            <node TEXT="PreorderTraversal"/>
+            <node TEXT="PostorderTraversal"/>
+            <node TEXT="LevelorderTraversal"/>
+            <node TEXT="Huffman Decoding"/>
+        </node>
+        <node TEXT="left" POSITION="left"/>
+        <node TEXT="data" POSITION="left"/>
+        <node TEXT="__topView__" POSITION="left"/>
+        <node TEXT="LinkedList" POSITION="left">
+            <node TEXT="add"/>
+            <node TEXT="isEmpty"/>
+            <node TEXT="poll"/>
+        </node>
+        <node TEXT="__postOrder__" POSITION="left"/>
+        <node TEXT="__preOrder__" POSITION="left"/>
+    </node>
+</map>
