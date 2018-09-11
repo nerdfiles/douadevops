911f6864b13af9efbd3a53790a48192c326fb43b
nerdfiles <nerdfiles@gmail.com>
2018-09-11 15:40:46 -0500

    Codic

---
+++ datastructures/SOLID-FUNCTIONS.mm
@@ -0,0 +1,53 @@
+
+<map version="0.9.0">
+    <node ID="ID_05B74FEF-1AE0-4802-832D-1F9AB5A2599A" TEXT="Codic">
+        <node ID="ID_99103CAD-D87D-487F-B700-79970C851884" TEXT="DIP (inversion of control)">
+            <node ID="ID_01A398CE-D554-4A00-B3EC-5A35563E2705" TEXT="Factory (Method) Pattern"/>
+            <node ID="ID_FA6854F5-491F-4C84-86C4-061C1B74A9D5" TEXT="Abstract Factory"/>
+            <node ID="ID_81229531-A5F1-4BA2-93E4-776F32CB7CC0" TEXT="Prototype"/>
+            <node ID="ID_0CDAFE74-C931-427A-BFBE-CF7B49A9249E" TEXT="Template (Method) Pattern"/>
+        </node>
+        <node ID="ID_E74805F2-62E2-4E44-A8C5-BE66EEE565F5" TEXT="Liskov Substitution Principle">
+            <node ID="ID_06E29736-D1B1-426F-938C-499DAF78DE64" TEXT="Builder"/>
+            <node ID="ID_2818B020-18C5-4188-9E5B-7A481D76BB0D" TEXT="Decorator"/>
+            <node ID="ID_EB8743C4-E790-4C18-86EB-F700F3F1D580" TEXT="Strategy"/>
+        </node>
+        <node ID="ID_B5819226-2042-4219-BCCC-275FFA27EBE5" TEXT="Composition (Reactive coding with streams, etc.)">
+            <node ID="ID_1381E0AF-0163-4CB1-A219-8EF3404C5C1A" TEXT="Rx.Observables, R.pipe"/>
+            <node ID="ID_C339723C-B675-4D02-BE13-DAC712B1DF3A" TEXT="Strategy"/>
+            <node ID="ID_AF707CF1-53EB-4AC6-A918-C399B22EF604" TEXT="Builder"/>
+        </node>
+        <node ID="ID_4AB824E0-280F-4764-B798-3C6D86DC93B0" TEXT="Single (Command|Query) Responsibility Principle">
+            <node ID="ID_0BD37368-5A74-486F-B1E1-AF686EFF1331" TEXT="Command"/>
+            <node ID="ID_BE1EE0B3-D4DD-4616-A76C-D1D021E77BBA" TEXT="Observer">
+                <node TEXT="Event Store"/>
+            </node>
+            <node ID="ID_541076B5-8E03-47D4-9723-AE3F4B838CB4" TEXT="CQRS: http://microservices.io/patterns/data/cqrs.html"/>
+        </node>
+        <node ID="ID_398F3905-1868-4AD0-8ADF-45E9D000B02D" TEXT="Open/Closed Principle">
+            <node ID="ID_CF0FDE68-64DC-47D1-8A72-512BCBC3B969" TEXT="Singleton"/>
+        </node>
+        <node ID="ID_FD56493A-2F38-4D0F-BC44-9349707C6916" TEXT="Conservation (Reuse bitemporal comparors, etc.)">
+            <node ID="ID_73FB5218-ED97-4F3F-81B5-990B39B95FBE" TEXT="Memento"/>
+        </node>
+        <node ID="ID_C6311848-8641-4CA5-935C-4A38157ABB65" TEXT="Interface Segregation Principle">
+            <node ID="ID_2C156C30-2B86-4BD9-ACCD-E3D9CB4A66D5" TEXT="Adapter"/>
+            <node ID="ID_5A42E137-EFE9-4406-B517-BAB05972D292" TEXT="Bridge"/>
+            <node ID="ID_B86C305B-7760-45E3-8D2C-BE7AD5E73666" TEXT="Memento"/>
+        </node>
+        <node ID="ID_851D1302-A537-4C8F-8F5C-6183C9D852D5" TEXT="Flow (Narration/SDRSs/Link Layer/etc Safety)">
+            <node ID="ID_807EECCF-D75F-436C-BA2C-B4DCCA79AF48" TEXT="(Amundsen determined by Paxos/Lamport, re: definition of technological aging)"/>
+            <node ID="ID_B67DEC82-CFB7-49DE-A995-674B0757B9E7" TEXT="Interpreter"/>
+        </node>
+        <node ID="ID_DD39A907-B01E-4B4F-BA0D-DEFE330FB932" TEXT="Separation (FOL URLs, class-based URL idempotence, etc.)">
+            <node ID="ID_EFBFABE6-2FB2-492B-A0DB-5E65BBB08309" TEXT="Command"/>
+        </node>
+        <node ID="ID_4EB4F4D1-9F4B-486D-A6C9-C47C3C8AE64A" TEXT="Immutability (Copy-on-write, Log Merge Trees, etc.)">
+            <node ID="ID_C296E936-3C70-4145-9E58-CD0A4CDEE881" TEXT="Strategy"/>
+        </node>
+        <node ID="ID_948B4368-404B-453D-87B6-6085C6B0E228" TEXT="Wisdom (Branching Executions) Principle (Truth Tables and Lists)">
+            <node ID="ID_4B5FBAAF-BB42-4691-B176-8DFD4A01473E" TEXT="Bridge"/>
+            <node ID="ID_B163400C-BF0F-4F6A-8264-86C5BC01F2EE" TEXT="Strategy"/>
+        </node>
+    </node>
+</map>
