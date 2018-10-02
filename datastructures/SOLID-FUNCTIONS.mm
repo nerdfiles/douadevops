22f992c0b0e2f911d45990860ad0355a3ba5de13
nerdfiles <nerdfiles@gmail.com>
2018-10-02 10:49:56 -0500

    Update

--- datastructures/SOLID-FUNCTIONS.mm
+++ datastructures/SOLID-FUNCTIONS.mm
@@ -14,18 +14,25 @@
         </node>
         <node ID="ID_B5819226-2042-4219-BCCC-275FFA27EBE5" TEXT="Composition (Reactive coding with streams, etc.)">
             <node ID="ID_1381E0AF-0163-4CB1-A219-8EF3404C5C1A" TEXT="Rx.Observables, R.pipe"/>
-            <node ID="ID_C339723C-B675-4D02-BE13-DAC712B1DF3A" TEXT="Strategy"/>
+            <node ID="ID_C339723C-B675-4D02-BE13-DAC712B1DF3A" TEXT="Strategy">
+                <node ID="ID_9FEF1572-B9F6-45AF-BA46-4E8268B793DD" TEXT="### Base Class StringSorter = (algorithm) -&gt;     sort: (list) -&gt; algorithm list">
+                    <font NAME="Anonymous Pro" SIZE="12"/>
+                </node>
+            </node>
             <node ID="ID_AF707CF1-53EB-4AC6-A918-C399B22EF604" TEXT="Builder"/>
         </node>
         <node ID="ID_4AB824E0-280F-4764-B798-3C6D86DC93B0" TEXT="Single (Command|Query) Responsibility Principle">
             <node ID="ID_0BD37368-5A74-486F-B1E1-AF686EFF1331" TEXT="Command"/>
             <node ID="ID_BE1EE0B3-D4DD-4616-A76C-D1D021E77BBA" TEXT="Observer">
-                <node TEXT="Event Store"/>
+                <node ID="ID_997F7065-7700-4252-9DF2-08D6C841CD7E" TEXT="Event Store"/>
             </node>
             <node ID="ID_541076B5-8E03-47D4-9723-AE3F4B838CB4" TEXT="CQRS: http://microservices.io/patterns/data/cqrs.html"/>
         </node>
         <node ID="ID_398F3905-1868-4AD0-8ADF-45E9D000B02D" TEXT="Open/Closed Principle">
             <node ID="ID_CF0FDE68-64DC-47D1-8A72-512BCBC3B969" TEXT="Singleton"/>
+            <node ID="ID_3B4F9179-2D6D-4325-9B24-C31231D67194" TEXT="Observer">
+                <node ID="ID_63808CB5-D916-439E-9AD9-2FDA365EF4B9" TEXT="Event Store"/>
+            </node>
         </node>
         <node ID="ID_FD56493A-2F38-4D0F-BC44-9349707C6916" TEXT="Conservation (Reuse bitemporal comparors, etc.)">
             <node ID="ID_73FB5218-ED97-4F3F-81B5-990B39B95FBE" TEXT="Memento"/>
@@ -43,11 +50,46 @@
             <node ID="ID_EFBFABE6-2FB2-492B-A0DB-5E65BBB08309" TEXT="Command"/>
         </node>
         <node ID="ID_4EB4F4D1-9F4B-486D-A6C9-C47C3C8AE64A" TEXT="Immutability (Copy-on-write, Log Merge Trees, etc.)">
-            <node ID="ID_C296E936-3C70-4145-9E58-CD0A4CDEE881" TEXT="Strategy"/>
+            <node ID="ID_C296E936-3C70-4145-9E58-CD0A4CDEE881" TEXT="Strategy">
+                <node ID="ID_19BC4AC6-27DA-40C7-B42A-CD8A5AECE2EE" TEXT="### Base Class StringSorter = (algorithm) -&gt;     sort: (list) -&gt; algorithm list">
+                    <font NAME="Anonymous Pro" SIZE="12"/>
+                </node>
+            </node>
         </node>
         <node ID="ID_948B4368-404B-453D-87B6-6085C6B0E228" TEXT="Wisdom (Branching Executions) Principle (Truth Tables and Lists)">
             <node ID="ID_4B5FBAAF-BB42-4691-B176-8DFD4A01473E" TEXT="Bridge"/>
-            <node ID="ID_B163400C-BF0F-4F6A-8264-86C5BC01F2EE" TEXT="Strategy"/>
+            <node ID="ID_B163400C-BF0F-4F6A-8264-86C5BC01F2EE" TEXT="Strategy">
+                <node ID="ID_D4CE8880-623B-4748-A48F-8552A529C051" TEXT="### Base Class ClientRater = (algorithm) -&gt;     rate: (list) -&gt; algorithm list">
+                    <font NAME="Anonymous Pro" SIZE="12"/>
+                </node>
+            </node>
         </node>
+        <node ID="ID_7CB1C7BC-F098-4510-9A74-C9708BFFDCA7" TEXT="Publish profiles" POSITION="left"/>
+        <node ID="ID_C2C11ED4-DAF5-4293-8B05-32AEA78657DD" TEXT="Pass messages, not objects" POSITION="left"/>
+        <node ID="ID_5A027B46-826E-4E06-8E84-7BDE161E82D5" TEXT="Share vocabularies, not modules" POSITION="left">
+            <node ID="ID_67F197AB-D9C4-40C1-B454-A14404822EF6" TEXT="ODRL"/>
+            <node ID="ID_74F002FA-F391-41B2-95BA-C0F149180154" TEXT="SHACL"/>
+        </node>
+        <node ID="ID_47587A5F-8F84-46EE-A63D-4E24AAF45453" TEXT="Representer Pattern" POSITION="left">
+            <node ID="ID_0F565199-8C98-43A9-9051-11AD3030FB3F" TEXT="Strategy">
+                <node ID="ID_4457DEBB-1B78-435C-A385-CF8A07770755" TEXT="### Base Class ClientRater = (algorithm) -&gt;     rate: (list) -&gt; algorithm list">
+                    <font NAME="Anonymous Pro" SIZE="12"/>
+                </node>
+            </node>
+            <node ID="ID_E0C1EA96-1014-4B71-A358-80C4D1E9C3A4" TEXT="function bind(orinFn,thisObj) {     return function boundFn(...args) {         return origFn.apply( thisObj, args );     }; }  var student = bind( StudentRecord, { name: &quot;Kyle..&quot; } ); ">
+                <font NAME="Anonymous Pro" SIZE="12"/>
+            </node>
+            <node ID="ID_99B549E6-69D6-4829-AB41-48E9CF43ED4A" TEXT="function StudentRecord(){     return `${this.name}, Major: ${this.major}, \ GPA: ${this.gpa.toFixed(1)}`; }  var student = StudentRecord.bind( {     name: &quot;Kyle Simpson&quot;,     major: &quot;CS&quot;,     gpa: 4 } );  // later  student(); // Kyle Simpson, Major: CS, GPA: 4.0">
+                <font NAME="Anonymous Pro" SIZE="12"/>
+            </node>
+        </node>
+        <node ID="ID_0634F570-5124-473E-90E6-241F5B57C345" TEXT="app.use({{{MUST_IGNORE}}})" POSITION="left"/>
+        <node ID="ID_1DBD49D4-AD0E-44CD-BB3B-75F0219F7F1F" TEXT="app.use({{{MUST_FORWARD}}})" POSITION="left"/>
+        <node ID="ID_2CD787DB-B58E-469A-9B9A-708F51080E3B" TEXT="app.use({{{PROVIDE_MRU}}})" POSITION="left"/>
+        <node ID="ID_7F61A545-DB44-43BA-8E42-D053E730A9AA" TEXT="app.use({{{IDEMPOTENCE}}})" POSITION="left"/>
+        <node ID="ID_3F3A7044-B0F7-4615-9E93-283A5ACF6500" TEXT="app.use({{{RELATED}}})" POSITION="left"/>
+        <node ID="ID_9F10C358-A17E-4F1F-BECF-10613275E528" TEXT="app.use({{{NAVIGATION}}})" POSITION="left"/>
+        <node ID="ID_5249D4CA-C988-4C1C-830D-CA56CC1DAF02" TEXT="app.use({{{partialSubmit}}})" POSITION="left"/>
+        <node ID="ID_239E47B7-9D72-4725-9E15-A71135F5A11B" TEXT="app.use({{{FINITE_STATE_MACHINE}}})" POSITION="left"/>
     </node>
 </map>
