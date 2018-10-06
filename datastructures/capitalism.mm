59e0730b103f75f891f18f874513c48ebbeeffad
nerdfiles <nerdfiles@gmail.com>
2018-10-06 01:56:21 -0500

    Update

---
+++ datastructures/capitalism.mm
@@ -0,0 +1,36 @@
+
+<map version="0.9.0">
+    <node TEXT="Capitalism Myths">
+        <node TEXT="Greed, a natural human instinct, makes markets work.">
+            <node TEXT="i.e., &quot;Greed is good&quot; is false.">
+                <font NAME="Arial Rounded MT Bold" SIZE="18"/>
+            </node>
+            <font NAME="Arial Rounded MT Bold" SIZE="24"/>
+        </node>
+        <node TEXT="Corporations must be run to maximize value for shareholders.">
+            <node TEXT="i.e., Corporations must be punished regularly.">
+                <font NAME="Arial Rounded MT Bold" SIZE="18"/>
+            </node>
+            <font NAME="Arial Rounded MT Bold" SIZE="24"/>
+        </node>
+        <node TEXT="Workers' pay is an objective measure of economic contribution.">
+            <node TEXT="i.e., Income entails happiness.">
+                <font NAME="Arial Rounded MT Bold" SIZE="18"/>
+            </node>
+            <font NAME="Arial Rounded MT Bold" SIZE="24"/>
+        </node>
+        <node TEXT="Equality of opportunity is all people need to climb the economic ladder." POSITION="left">
+            <node TEXT="&quot;Racial equity is the condition that would be achieved if one's racial identity no longer predicted, in a statistical sense, how one fares.&quot; (Source: Center for Assessment and Policy Development) http://www.racialequityresourceguide.org/about/glossary">
+                <font NAME="Arial Rounded MT Bold" SIZE="18"/>
+            </node>
+            <font NAME="Arial Rounded MT Bold" SIZE="24"/>
+        </node>
+        <node TEXT="Making the economy fairer will make it smaller and less prosperous." POSITION="left">
+            <node TEXT="i.e., High debt does not entail low growth.">
+                <font NAME="Arial Rounded MT Bold" SIZE="18"/>
+            </node>
+            <font NAME="Arial Rounded MT Bold" SIZE="24"/>
+        </node>
+        <font NAME="Arial Rounded MT Bold" SIZE="24"/>
+    </node>
+</map>
---
+++ datastructures/ddd.mm
@@ -0,0 +1,45 @@
+
+<map version="0.9.0">
+    <node TEXT="DDD">
+        <node TEXT="test/"/>
+        <node TEXT="src/">
+            <node TEXT="app/">
+                <node TEXT="user/"/>
+                <node TEXT="account/"/>
+                <node TEXT="company/"/>
+            </node>
+            <node TEXT="domain/">
+                <node TEXT="user/"/>
+                <node TEXT="account/"/>
+                <node TEXT="company/"/>
+            </node>
+            <node TEXT="infrastructure/">
+                <node TEXT="database/"/>
+                <node TEXT="encryption/"/>
+                <node TEXT="jwt/"/>
+                <node TEXT="logging/"/>
+                <node TEXT="repos/"/>
+                <node TEXT="sequelize/"/>
+                <node TEXT="support/"/>
+            </node>
+            <node TEXT="interfaces/">
+                <node TEXT="http://"/>
+                <node TEXT="quic"/>
+                <node TEXT="udp"/>
+                <node TEXT="{sound_driver}"/>
+                <node TEXT="ws://"/>
+                <node TEXT="https://"/>
+                <node TEXT="net"/>
+                <node TEXT="socket"/>
+                <node TEXT="os"/>
+                <node TEXT="events"/>
+            </node>
+        </node>
+        <node TEXT="config/"/>
+        <node TEXT="index.js"/>
+        <node TEXT="public/" POSITION="left"/>
+        <node TEXT="logs/" POSITION="left"/>
+        <node TEXT="Procfile" POSITION="left"/>
+        <node TEXT="package.json" POSITION="left"/>
+    </node>
+</map>
